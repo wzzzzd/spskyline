3f95500fe75889442443497a631f9bd6e8c5b417
wzzzzd <wzzzzd@proton.me>
2023-05-25 22:30:09 +0800

    update

---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+#![feature(is_some_and)]
+
+use std::cmp::Ordering;
+use std::collections::{HashMap, VecDeque};
+use std::hash::Hash;
+
+use num_traits::bounds::UpperBounded;
+use num_traits::{One, Zero};
+use petgraph::algo::BoundedMeasure;
+use petgraph::prelude::DiGraphMap;
+use petgraph::visit::{
+    Bfs, IntoNeighbors, IntoNeighborsDirected, IntoNodeIdentifiers, IntoNodeReferences, NodeCount,
+    Visitable,
+};
+use petgraph::Direction;
+
+/// Main query function. Each entry of `node_to_keyword` should be sorted.
+pub fn semantic_place_skyline<G, K, D>(
+    graph: G,
+    node_to_keyword: &HashMap<G::NodeId, Vec<K>>,
+    keywords: &[K],
+) -> Vec<(G::NodeId, Vec<D>)>
+where
+    G: IntoNodeIdentifiers + IntoNeighborsDirected,
+    G::NodeId: Hash + Ord,
+    K: Ord,
+    D: Copy + Ord + Zero + One + UpperBounded,
+{
+    // at least one keyword should be provided
+    assert!(!keywords.is_empty());
+    // initialize keyword distances
+    let mut dists: HashMap<_, _> = graph
+        .node_identifiers()
+        .map(|node| (node, vec![D::max_value(); keywords.len()]))
+        .collect();
+    // for each keyword, calculate the distance from each node to the nodes containing it
+    // implemented by multi-source bfs
+    for (keyword_idx, keyword) in keywords.iter().enumerate() {
+        let mut queue: VecDeque<_> = graph
+            .node_identifiers()
+            .filter(|node| {
+                node_to_keyword
+                    .get(node)
+                    .is_some_and(|node_keywords| node_keywords.binary_search(keyword).is_ok())
+            })
+            .collect();
+        for node in &queue {
+            *dists.get_mut(node).unwrap().get_mut(keyword_idx).unwrap() = D::zero();
+        }
+        while let Some(current) = queue.pop_front() {
+            let dist = dists[&current][keyword_idx];
+            for nbr in graph.neighbors_directed(current, Direction::Incoming) {
+                let nbr_dist = dists
+                    .get_mut(&nbr)
+                    .and_then(|v| v.get_mut(keyword_idx))
+                    .unwrap();
+                if dist + D::one() < *nbr_dist {
+                    *nbr_dist = dist + D::one();
+                    queue.push_back(nbr);
+                }
+            }
+        }
+    }
+    // find the minimal elements in the partially ordered set
+    dists
+        .iter()
+        .filter(|(_, du)| {
+            dists
+                .values()
+                .all(|dv| partial_cmp(dv, du) != Some(Ordering::Less))
+        })
+        .map(|(u, du)| (*u, du.clone()))
+        .collect()
+}
+
+fn partial_cmp<D: Ord>(dv1: &[D], dv2: &[D]) -> Option<Ordering> {
+    assert_eq!(dv1.len(), dv2.len());
+    if dv1.is_empty() {
+        return Some(Ordering::Equal);
+    }
+    let (dv1_first, dv1) = dv1.split_first().unwrap();
+    let (dv2_first, dv2) = dv2.split_first().unwrap();
+    match dv1_first.cmp(dv2_first) {
+        Ordering::Less => dv1
+            .iter()
+            .zip(dv2)
+            .all(|(d1, d2)| d1 <= d2)
+            .then_some(Ordering::Less),
+        Ordering::Greater => dv1
+            .iter()
+            .zip(dv2)
+            .all(|(d1, d2)| d1 >= d2)
+            .then_some(Ordering::Greater),
+        Ordering::Equal => partial_cmp(dv1, dv2),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,108 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(iterator_try_collect)]
+
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
+use std::time::Instant;
+
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use petgraph::prelude::DiGraphMap;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to the edge file
+    #[arg(short)]
+    edge_file: PathBuf,
+
+    /// Path to the node keyword file
+    #[arg(short)]
+    node_keyword_file: PathBuf,
+
+    /// List of keyword sets delimited by space. Example: "1,2,3 4,5,6"
+    #[arg(value_parser = parse_keywords)]
+    queries: Vec<Vec<u32>>,
+}
+
+fn parse_keywords(s: &str) -> Result<Vec<u32>> {
+    Ok(s.split(',').map(|k| k.parse()).try_collect()?)
+}
+
+fn build_graph(
+    edge_file_path: &Path,
+    node_keyword_file_path: &Path,
+) -> Result<(DiGraphMap<u32, ()>, HashMap<u32, Vec<u32>>)> {
+    let edge_file = File::open(edge_file_path)?;
+    let node_keyword_file = File::open(node_keyword_file_path)?;
+    let mut graph = DiGraphMap::new();
+    let reader = BufReader::new(edge_file);
+    for (line_number, line) in reader.lines().enumerate() {
+        let line = line?;
+        let (source, targets) = line.split_once(':').ok_or(anyhow!(
+            "expect ':' at line {} in {}",
+            line_number + 1,
+            edge_file_path.display()
+        ))?;
+        let source = source.parse()?;
+        for target in targets
+            .trim_matches(|x: char| x.is_whitespace() || x == ',')
+            .split(',')
+            .map(|t| t.parse::<u32>())
+        {
+            let target = target?;
+            if graph.add_edge(source, target, ()).is_some() {
+                return Err(anyhow!(
+                    "duplicate edge found: source: {}, target: {}.",
+                    source,
+                    target
+                ));
+            }
+        }
+    }
+    let mut node_to_keyword = HashMap::new();
+    let reader = BufReader::new(node_keyword_file);
+    for (line_number, line) in reader.lines().enumerate() {
+        let line = line?;
+        let (node, keywords) = line.split_once(':').ok_or(anyhow!(
+            "expect ':' at line {} in {}",
+            line_number + 1,
+            node_keyword_file_path.display()
+        ))?;
+        let node = node.parse()?;
+        let mut keywords: Vec<_> = keywords
+            .trim_matches(|x: char| x.is_whitespace() || x == ',')
+            .split(',')
+            .map(|t| t.parse::<u32>())
+            .try_collect()?;
+        keywords.sort_unstable();
+        if node_to_keyword.insert(node, keywords).is_some() {
+            return Err(anyhow!("duplicate node found: {}.", node));
+        }
+    }
+    Ok((graph, node_to_keyword))
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let start = Instant::now();
+    let (graph, node_to_keyword) = build_graph(&args.edge_file, &args.node_keyword_file)?;
+    let building_time = start.elapsed();
+    println!("Building graph: {}", building_time.as_secs_f64());
+
+    for keywords in args.queries {
+        let start = Instant::now();
+        let result =
+            skyline::semantic_place_skyline::<_, _, u32>(&graph, &node_to_keyword, &keywords);
+        let exec_time = start.elapsed();
+        println!("Keywords: {:?}", keywords);
+        println!("Execution time: {}", exec_time.as_secs_f64());
+        for (root, dist) in result {
+            for (k, d) in keywords.iter().zip(dist) {
+                println!("{}: {} distance {}", root, k, d);
+            }
+        }
+        println!();
+    }
+    Ok(())
 }
